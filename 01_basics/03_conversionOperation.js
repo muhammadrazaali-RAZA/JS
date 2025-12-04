e9af717cae0ac2ed9e9c998cbadc0175398b632c
Muhammad Raza Ali 12248246 <muhammadra98@almighty.cs.univie.ac.at>
2025-12-04 21:04:10 +0100

    Notes of JavaScript for Future use,have all basics and containing all concepts.

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,12 @@
+let score = "33";
+
+console.log(score);
+console.log(typeof score);
+
+score = "33abc";
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
