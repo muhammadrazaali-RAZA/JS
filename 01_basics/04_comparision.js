33c45492ec637713cf49003566446284334a560b
Muhammad Raza Ali 12248246 <muhammadra98@almighty.cs.univie.ac.at>
2025-12-04 23:21:46 +0100

    Conversion of dataTypes with Investigation Notes;

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+// basic conversion
+console.log(2 > 1);      // true
+console.log(2 >= 1);     // true
+console.log(2 < 1);      // false
+console.log(2 == 1);     // false
+console.log(2 != 1);     // true
+
+// better to avoid the confusion check
+console.log("2" > 1);       // true
+console.log("02" > 1);      // true
+
+console.log(null > 0);      // false  ;(because 0 > 0 is false)
+console.log(null == 0);     // false  ;(because null only equals undefined)
+console.log(null >= 0);     // true   ;(because 0 >= 0 is true)
+
+console.log(undefined == 0);// false
+console.log(undefined > 0); // false
+console.log(undefined < 0); // false
+
+// strict check ===
+
+console.log("2" === 2);
---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,45 @@
+// # Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Dynamic Language
+
+const score = 100;
+const scoreValue = 5.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+
+let userEmail;
+
+let id = Symbol('123');
+let anotherId = Symbol('123');
+
+console.log(id === anotherId); // false  --always unique, even with the same description.
+
+id = Symbol.for('123');
+anotherId = Symbol.for('123');
+
+console.log(id === anotherId); // true   --reuses the same symbol from a global registry.
+
+const bigNumber = 32165465486354n
+console.log(typeof bigNumber);
+
+
+// # Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+
+const heros = ["ironman", "spiderman", "hulk"];
+
+let myObj = {
+    name: "Raza",
+    age: 27,
+};
+
+const myFunction = function(){
+    console.log("Hello World!");
+}
+
+console.log(typeof myFunction);   // function
